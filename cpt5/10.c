4e9d85caf506dea61ff87e1c17d1f60c7b240afe
albert_fe <albert.chg9@gmail.com>
2024-10-24 17:43:44 +0800

    v7

--- cpt5/10.c
+++ cpt5/10.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
-int main{
-    
+int fibonacci(int f){
+    if (f <= 0) return 0;
+    if (f == 1) return 1;
+    return fibonacci(f - 1) + fibonacci(f - 2);
+}
+
+int main(){
+    float sum = 0, temSum = 0;
+    for (int i = 1; i <= 20; i++){
+        temSum = fibonacci(i + 2) / fibonacci(i + 1);
+        sum += temSum;
+    }
+    printf("结果是：%f\n", sum);
+
 }
